e27d18be045216320b91f217a85be66fcb36cfa3
Bailey Miles <baileyleah542@gmail.com>
2023-05-07 02:14:55 +0100

    latest stuff

--- Assets/scripts/VersionChecker.cs
+++ Assets/scripts/VersionChecker.cs
@@ -4,7 +4,7 @@ using UnityEngine.Networking;
 
 public class VersionChecker : MonoBehaviour
 {
-    private const string CURRENT_VER = "V2.0";
+    private const string CURRENT_VER = "V2.1";
     public static bool correctVersion = true;
     [SerializeField] GameObject loadingScreen;
 
